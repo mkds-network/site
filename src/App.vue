21f6403f56b60db51efcda9214fed8e735bb66ef
JVDev <jason.vandaele.pro@gmail.com>
2023-05-29 17:59:43 +0200

    Setting the basic form of the website

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <NavigationComponent></NavigationComponent>
-  <MainComponent></MainComponent>
+  <div class="container-fluid" style="height:100%;">
+    <div class="row" style="height:100%;">
+      <div class="col-1" style="padding: 0px">
+        <NavigationComponent></NavigationComponent>
+      </div>
+      <div class="col-11" style="padding: 0px;">
+        <MainComponent></MainComponent>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -17,24 +25,21 @@ export default {
 </script>
 
 <style>
+html {
+  height: 100%;
+}
+
+body {
+  padding: 0px;
+  height: 100%;
+}
+
 #app {
+  width: 100%;
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- src/views/TrackDetailsView.vue
+++ src/views/TrackDetailsView.vue
@@ -1,53 +1,59 @@
 <template>
-    <div class="trackDetails">
-      <span>Hi, I'm the track details view !</span>
-      <table border="1">
-        <thead>
+  <div class="trackDetails">
+    <span>Hi, I'm the track details view !</span>
+    <table id="ranking-table-details">
+      <thead>
+        <tr>
+          <th>Rank</th>
+          <th>Player</th>
+          <th>Time</th>
+          <th v-if="this.times != null && Object.keys(this.times[0]).includes('std')">Standard</th>
+          <th>Country</th>
+        </tr>
+      </thead>
+      <tbody v-for="time in this.times" v-bind:key="time.rank">
           <tr>
-            <th>Rank</th>
-            <th>Player</th>
-            <th>Time</th>
-            <th v-if="this.times != null && Object.keys(this.times[0]).includes('std')">Standard</th>
-            <th>Country</th>
+              <td>{{ time['rank'] }}</td>
+              <td>{{ time['player'] }}</td>
+              <td v-if="time['video'] != null" v-bind:style="[time['isCr?'] ? {'color':'lime'} : {'color':'inherit'}]">
+                  <a :href="`${ time['video'] }`" target="_blank" v-bind:style="'color: inherit'">{{ time['time'] }}</a>
+              </td>
+              <td v-else v-bind:style="[time['isCr?'] ? {'color':'lime'} : {'color':'inherit'}]">{{ time['time'] }}</td>
+              <td v-if="Object.keys(time).includes('std')">{{ time['std'] }}</td>
+              <td>{{ time['country'] }}</td>
           </tr>
-        </thead>
-        <tbody v-for="time in this.times" v-bind:key="time.rank">
-            <tr>
-                <td>{{ time['rank'] }}</td>
-                <td>{{ time['player'] }}</td>
-                <td v-if="time['video'] != null" v-bind:style="[time['isCr?'] ? {'background-color':'lime'} : {'background-color':'white'}]">
-                    <a :href="`${ time['video'] }`" target="_blank">{{ time['time'] }}</a>
-                </td>
-                <td v-else v-bind:style="[time['isCr?'] ? {'background-color':'lime'} : {'background-color':'white'}]">{{ time['time'] }}</td>
-                <td v-if="Object.keys(time).includes('std')">{{ time['std'] }}</td>
-                <td>{{ time['country'] }}</td>
-            </tr>
-        </tbody>
-      </table>
-    </div>
-  </template>
+      </tbody>
+    </table>
+  </div>
+</template>
     
     
-  <script>
-  import { useRoute } from 'vue-router';
+<script>
+import { useRoute } from 'vue-router';
   
-  export default {
-    name: 'TrackDetailsView',
-    data() {
-      return {
-          times: null,
-      }
-    },
-    methods: {
-        getTrackData : async function () {
-            const route = useRoute()
-            this.times = await import(`../data/times/${ route.params.extension }/${ route.params.category }/${ route.params.format }/${ route.params.track }.json`).then(module => {
-                return module.default;
-            });
-        }
-    },
-    async created(){
-        await this.getTrackData();
+export default {
+  name: 'TrackDetailsView',
+  data() {
+    return {
+        times: null,
     }
+  },
+  methods: {
+      getTrackData : async function () {
+          const route = useRoute()
+          this.times = await import(`../data/times/${ route.params.extension }/${ route.params.category }/${ route.params.format }/${ route.params.track }.json`).then(module => {
+              return module.default;
+          });
+      }
+  },
+  async created(){
+      await this.getTrackData();
   }
-  </script>
+}
+</script>
+
+<style scoped>
+#ranking-table-details {
+  width: 100%;
+}
+</style>
