40ada036c125148edfb2c3bd679b21dcbb4eddea
JVDev <jason.vandaele.pro@gmail.com>
2023-08-17 19:58:45 +0200

    Just building the dist folder along with the modifications

---
+++ js/490.9341a42c.js
@@ -0,0 +1,1 @@
+"use strict";(self["webpackChunksite"]=self["webpackChunksite"]||[]).push([[490],{3490:function(e){e.exports=JSON.parse('[{"rank":1,"player":"Xanix","country":"DevIsland","time":"98:76:543","std":"NW","proof":null,"isCr?":true}]')}}]);
